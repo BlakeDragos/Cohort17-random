25f1065363fce76259a5aba79c712ffb76ba008a
BlakeDragos <blakedragos@gmail.com>
2019-09-16 21:08:39 -0500

    cant complain now

--- logic.js
+++ logic.js
@@ -1,28 +1,54 @@
 $(document).ready(function () {
-    const classroom = ["Abdul Barre", "Alex Griep", "Ashley Wegwerth", "Aubrey Koski", "Balaji Manoharan", "Baraka Ibrahim",
-        "Bashir Raghe", "Ben Honken", "Blake Pierce", "Brennan Spicer", "Brooke Kumar", "Bryan Iund", "Colin Kramer", "Elizabeth O'Leary",
-        "Hamza Abdikarim", "Henry Johnson", "Issa Issa", "Jacob Rosenbaum", "James Botham", "James Caples", "Johnny Yang", "Ka Vang",
-        "Kayla Kuhlman", "Keith Kleinschmidt", "Kyle Betlach", "Mai Xiong", "Mauricio Fuentes gomez", "Mohamed Abdi", "Mohamed Ahmed",
-        "Ranji Ramroop", "Tasha Tran", "Terri Decaire", "Tim Scheve", "Yogeeta Gajway", "Zhen yong Chen"];
+    let classroom;
+    let current;
+
     function shuffle(array) {
         array.sort(() => Math.random() - 0.5);
     }
-    let current = 0;
-    $("#random").click(function(){
+
+    if (localStorage.getItem("classroom") != null) {
+        classroom = JSON.parse(localStorage.getItem("classroom"));
+        current = parseInt(localStorage.getItem("current"));
+        $("#random").hide();
+        $("#student").text(classroom[current]);
+    }else{
+        classroom = ["Abdul Barre", "Alex Griep", "Ashley Wegwerth", "Aubrey Koski", "Balaji Manoharan", "Baraka Ibrahim",
+        "Bashir Raghe", "Ben Honken", "Blake Pierce", "Brennan Spicer", "Brooke Kumar", "Bryan Iund", "Colin Kramer", "Elizabeth O'Leary",
+        "Hamza Abdikarim", "Henry Johnson", "Issa Issa", "Jacob Rosenbaum", "James Botham", "James Caples", "Johnny Yang", "Ka Vang",
+        "Kayla Kuhlman", "Keith Kleinschmidt", "Kyle Betlach", "Mai Xiong", "Mauricio gomez", "Mohamed Abdi", "Mohamed Ahmed",
+        "Ranji Ramroop", "Tasha Tran", "Terri Decaire", "Tim Scheve", "Yogeeta Gajway", "Zhen yong Chen"];
+        current = 0;
+    }
+    $("#random").click(function () {
         current = 0;
         shuffle(classroom);
         console.log(classroom);
         $("#student").text("Start");
+        $("#random").hide();
     })
-    console.log(classroom.length);
+
     $("#student").click(function () {
-        $("#student").text(classroom[current]);
-        current++;
-        if (current > classroom.length) {
+        if (current >= classroom.length) {
             current = 0;
+            shuffle(classroom);
+            console.log(classroom);
+            $("#student").text("Start");
+            localStorage.setItem("classroom", JSON.stringify(classroom));
+            localStorage.setItem("current", JSON.stringify(current));
+        } else if(current === 0){
             $("#student").text(classroom[current]);
+            localStorage.setItem("classroom", JSON.stringify(classroom));
+            localStorage.setItem("current", JSON.stringify(current));
+            current++;
+        } 
+        else {
+            $("#student").text(classroom[current]);
+            current++;
+            localStorage.setItem("classroom", JSON.stringify(classroom));
+            localStorage.setItem("current", JSON.stringify(current));
         }
 
-    })
+
+    });
 
 });
