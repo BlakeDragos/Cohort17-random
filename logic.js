9ac60528b59c8d2c01b763c58ccf02bb49b5fd1d
BlakeDragos <blakedragos@gmail.com>
2019-10-05 13:14:11 -0500

    added the two students I was missing

--- logic.js
+++ logic.js
@@ -14,16 +14,17 @@ $(document).ready(function () {
     } else {
         classroom = ["Abdul Barre", "Alex Griep", "Ashley Wegwerth", "Aubrey Koski", "Balaji Manoharan", "Baraka Ibrahim",
             "Bashir Raghe", "Ben Honken", "Blake Pierce", "B Spice", "Brooke Kumar", "Bryan Iund", "Colin Kramer", "Elizabeth O'Leary",
-            "Hamza Abdikarim", "Henry Johnson", "Issa Issa", "Jacob Rosenbaum", "James Botham", "James Caples", "Johnny Yang", "Ka Vang",
+            "Hamza Abdikarim", "Henry Johnson", "Issa Issa", "Jacob Rosenbaum", "Jonathan Carrasco", "Elia Facundo Orta Carrasco", "James Botham", "James Caples", "Johnny Yang", "Ka Vang",
             "Kayla Kuhlman", "Keith Kleinschmidt", "Kyle Betlach", "Mai Xiong", "Mauricio Gomez", "Mohamed Abdi", "Mohamed Ahmed",
-            "Ranji Ramroop", "Tasha Tran", "Terri Decaire", "Tim Scheve", "Yogeeta Gajway", "Zhen yong Chen", "Abdullahi Hassans"];
+            "Ranji Ramroop", "Tasha Tran", "Terri Decaire", "Tim Scheve","Richard Tshabalala", "Yogeeta Gajway", "Zhen yong Chen", "Abdullahi Hassans"];
         current = 0;
+        console.log(classroom)
     }
     $("#random").click(function () {
         current = 0;
         shuffle(classroom);
         console.log(classroom);
-        $("#student").text(classroom[current] + ": " + (current + 1));
+        $("#student").text(classroom[current] + ": " + current + 1);
         $("#random").addClass('animated bounceOutUp');
         setTimeout(function () {
             $("#random").hide();
